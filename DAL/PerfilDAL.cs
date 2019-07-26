aae3cba0e37f6c0c9d2b1571d5e96d54520464f2
Rafael Luge <rafael.rla91@gmail.com>
2019-07-26 15:17:08 -0300

    aula 13

---
+++ DAL/PerfilDAL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using System.Data;
+using Npgsql;
+
+namespace DAL
+{
+    public class PerfilDAL
+    {
+        public void SalvarImagem(Perfil perfil) 
+        {
+            try
+            {
+                String salvarI = (String.Format("UPDATE USUARIOS_CONFIG" +
+                    "SET VALOR = '{0}', " +
+                    "PLANO_DE_FUNDO = '{1}'" +
+                    "WHERE LOGIN = '{2}'", perfil.Imagem, 'I', perfil.Login));
+
+                NpgsqlCommand comandoUpdate = new NpgsqlCommand
+                (salvarI, ConnectionFactory.connect());
+                comandoUpdate.ExecuteNonQuery();
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception("Falha ao salvar o caminho da Imagem!" + ex.Message)
+            }
+
+            finally
+            {
+                ConnectionFactory.connect().Close();
+            }
+        }
+    }
+}
---
+++ Models/Perfil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Perfil
+    {
+        private string login, plano_de_fundo, descricao, valor, cor, imagem;
+
+        public string Imagem 
+            {
+                get { return imagem;}
+                set { imagem = value;}
+            }
+
+        public string Cor
+            {
+                get { return cor; }
+                set { cor = value; }
+            }
+        public string Valor
+            {
+                get { return valor;}
+                set {valor = value;}
+            }
+        public string Descricao
+            {
+                get { return descricao; }
+                set { descricao = value; }
+            }
+        public string Plano_de_Fundo 
+            {
+                get { return plano_de_fundo; }
+                set { plano_de_fundo = value; }
+            }
+        public string Login 
+            {
+                get { return login; }
+                set { login = value; }
+            }
+
+       
+    }
+}
